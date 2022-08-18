fdb8d8ad1f287bed70223ae1a43274a9b3ac187d
suchulchoi <tncjf856@naver.com>
2022-08-18 16:23:08 +0845

    hrn-react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./component/Navbar";
+import ProductAll from "./page/ProductAll";
+import Login from "./page/Login";
+import PrivateRoute from "./Route/PrivateRoute";
 
 function App() {
+  let [authenticate, setAuthenticate] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar authenticate={authenticate} setAuthenticate={setAuthenticate} />
+      <Routes>
+        <Route path="/" element={<ProductAll />} />
+        <Route
+          path="/login"
+          element={<Login setAuthenticate={setAuthenticate} />}
+        />
+        <Route
+          path="/product/:id"
+          element={<PrivateRoute authenticate={authenticate} />}
+        />
+      </Routes>
     </div>
   );
 }
